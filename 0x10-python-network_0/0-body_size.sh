e6b53eaf63279cfe56278ec5b917435d31e42b0c
Eli-wisdom <eliwisdom05@gmail.com>
2023-11-30 12:38:21 +0000

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+# Use curl to send a request and store the response in a temporary file
+response_file=$(mktemp)
+curl -sS "$url" > "$response_file"
 
+# Get the size of the response body in bytes
+size_in_bytes=$(stat --printf="%s" "$response_file")
+
+# Display the size
+echo "Size of the response body: $size_in_bytes bytes"
+
+# Clean up: Remove the temporary file
+rm "$response_file"
