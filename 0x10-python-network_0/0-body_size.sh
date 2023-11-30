7e1f134c5efe5aa71e7704be32a5d24fc8ac105e
Eli-wisdom <eliwisdom05@gmail.com>
2023-11-30 15:06:41 +0000

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Set the URL
-url="https://www.example.com"
-
-# Set the output file
-output_file="response.txt"
-
 # Make a GET request, save the response to a file, and display the size
-size=$(curl -sS -o "$output_file" -w "%{size_download}" "$url")
+size=$(curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
+)
 
 echo $size
-
 
