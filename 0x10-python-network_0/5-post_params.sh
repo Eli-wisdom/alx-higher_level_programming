9ed143133a6a984b1f9ae39b9a2a443c54b8c4fc
Eli-wisdom <eliwisdom05@gmail.com>
2023-11-30 22:07:38 +0000

    add

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#takes in a URL, sends a POST request to the passed URL
+#takes in a URL
 
 result=$(curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD")
 echo $result
