68d439c2f178fee511d2aea287ea1d0fbda86e6c
Eli-wisdom <eliwisdom05@gmail.com>
2023-11-30 22:56:40 +0000

    add

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed
-curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
+# sends a JSON POST request
+result=$(curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2")
+echo $result
 
