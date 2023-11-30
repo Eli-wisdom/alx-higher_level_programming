bdec81211f66f15f486167f8167cc2e68a020cc3
Eli-wisdom <eliwisdom05@gmail.com>
2023-11-30 15:12:05 +0000

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Make a GET request, save the response to a file, and display the size
-size=$(curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
-)
-
-echo $size
-
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
