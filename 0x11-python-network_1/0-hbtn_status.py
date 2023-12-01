01ca924a39db41be30cdac0c7f9dae4ce6d8593a
Eli-wisdom <eliwisdom05@gmail.com>
2023-12-01 21:41:29 +0000

    add

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+url='https://alx-intranet.hbtn.io/status'
+with urllib.request.openurl(url) as respond:
+	body = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+  	print('\t- utf8 content: {}'.format(body.decode('utf-8')))
