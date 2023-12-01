14471027dbc31617d0216d07cc419147a61f2571
Eli-wisdom <eliwisdom05@gmail.com>
2023-12-01 22:08:45 +0000

    add

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+url_ = sys.argv[1]
+email_ = {'email': sys.argv[2]}
+email = urllib.parse.urlencode(email_)
+email = email.encode('ascii')
+url_req = urllib.request.Request(url_, email)
+
+with urllib.request.urlopen(url_req) as response:
+    url_res = response.read()
+output = url_res.decode('utf-8')
+print(output)
+
