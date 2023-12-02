0c764db41c07d5e5c15a04cd53f624ad760d206a
Eli-wisdom <eliwisdom05@gmail.com>
2023-12-02 04:46:01 +0000

    in

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""sends a request to the URL and displays the body of the response."""
+"""sends a request to the URL and display"""
 
-if __name__ == "__main__":
-    import sys
-    import requests
+if __name__ == '__main__':
+	import sys
+	import requests
 
-    response = requests.get(sys.argv[1])
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
-    else:
-        print(response.text)
+	response = requests.get(sys.argv[1])
+	if response.status_code >= 400:
+		print("Error code: {}".format(response.status_code))
+	else:
+		print(response.text)
